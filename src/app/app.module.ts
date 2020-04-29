c3f3b71b6289780364c1cb00d7d6a8048e24447a
Nick Dunn <nick.dunn@flosports.tv>
2020-04-29 09:17:48 -0500

    add stubbed inputToCountdown directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component'
 import { LetterComponent } from './components/letter/letter.component'
 import { CountdownComponent } from './components/countdown/countdown.component'
 import { TimeInputComponent } from './components/time-input/time-input.component'
+import { InputToCountdownDirective } from './directives/input-to-countdown/input-to-countdown.directive'
 
 @NgModule({
   declarations: [
     AppComponent,
     CountdownComponent,
+    InputToCountdownDirective,
     LetterComponent,
     TimeInputComponent
   ],
---
+++ src/app/directives/input-to-countdown/input-to-countdown.directive.ts
@@ -0,0 +1,8 @@
+import { Directive } from "@angular/core";
+
+@Directive({
+    selector: '[scattInputToCountdown]'
+})
+export class InputToCountdownDirective {
+
+}

e9db6292ce071d987bd518b698ae16c0fb6574bc
Nick Dunn <nick.dunn@flosports.tv>
2020-04-29 09:12:08 -0500

    move all components to new directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 
 import { AppComponent } from './app.component'
-import { LetterComponent } from './letter/letter.component'
-import { CountdownComponent } from './countdown/countdown.component'
-import { TimeInputComponent } from './time-input/time-input.component'
+import { LetterComponent } from './components/letter/letter.component'
+import { CountdownComponent } from './components/countdown/countdown.component'
+import { TimeInputComponent } from './components/time-input/time-input.component'
 
 @NgModule({
   declarations: [
